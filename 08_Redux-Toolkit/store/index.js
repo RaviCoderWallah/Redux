2fb6bf02f4f4ba7c11f415f1ef59dec4f35461ab
RaviCoderWallah <raviverma05042023@gmail.com>
2026-05-13 16:40:12 +0530

    middleware in redux.

--- 08_Redux-Toolkit/store/index.js
+++ 08_Redux-Toolkit/store/index.js
@@ -3,10 +3,19 @@ import cartReducer from "./slices/cartSlice.js";
 import whishListReducer from "./slices/whishListSlice.js";
 import { configureStore } from "@reduxjs/toolkit";
 
+function logger(store) {
+  return function (next) {
+    return function (action) {
+      next(action);
+    };
+  };
+}
+
 export const store = configureStore({
   reducer: {
     productList: productsReducer,
     cartItems: cartReducer,
     whishList: whishListReducer,
   },
+  middleware: () => [logger],
 });
